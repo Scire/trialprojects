fb1da811e2ddc0bc68bbc78c60769a3eb8e50e42
Scire <anders.sjodin@hotmail.com>
2014-08-24 18:16:25 +0200

    First check in

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> users = new Dictionary<string, string>();
+            users.Add("Anders", "banan");
+            users.Add("Gustaf", "apelsin");
+            users.Add("Klara", "päron");
+            
+           
+            string userName = null;
+            string userPassword = null;
+            while (true)
+            {
+                Console.Write("User Name: ");
+                userName = Console.ReadLine();           
+                if (users.ContainsKey(userName))
+                {
+                    string correctPassword = users[userName].ToString();
+                    while (true)
+                    {
+                        Console.Write("Password: ");
+                        userPassword = Console.ReadLine();
+                        if (userPassword == correctPassword)
+                        {
+                            Console.WriteLine("Welcome");
+                            Console.ReadLine();
+                            return;
+                        }
+                        else
+                        {
+                            // Returns admonishment if user is stupid                           
+                            Console.WriteLine("Wrong Password");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Wrong User!");
+                }
+            } 
+        }
+    }
+}
